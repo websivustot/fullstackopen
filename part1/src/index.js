6f0cb37110860cfbacf0122d6a7037dd6f56f3c2
Alex <websivustot@gmail.com>
2019-12-05 12:49:15 +0200

    part1 step2

--- part1/src/index.js
+++ part1/src/index.js
@@ -11,15 +11,9 @@ const Content = (props) => {
 
     return (
         <>
-        <p>
-        {props.part[0]} {props.exercises[0]}
-      </p>
-      <p>
-        {props.part[1]} {props.exercises[1]}
-      </p>
-      <p>
-        {props.part[2]} {props.exercises[2]}
-      </p>
+        <Part part={props.part[0]} exercises={props.exercises[0]} />
+        <Part part={props.part[1]} exercises={props.exercises[1]} />
+        <Part part={props.part[2]} exercises={props.exercises[2]} />        
       </>
     )
 }
@@ -27,6 +21,14 @@ const Content = (props) => {
 const Total = (props) => {
     return (
         <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <p>
+            {props.part} {props.exercises}
+        </p>
     )
 }
 
@@ -43,8 +45,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content part={[part1,part2,part3]} 
-                exercises={[exercises1,exercises2,exercises3]}                 
-      />
+                exercises={[exercises1,exercises2,exercises3]} />
       <Total exercises={[exercises1,exercises2,exercises3]} />      
     </div>
   )
