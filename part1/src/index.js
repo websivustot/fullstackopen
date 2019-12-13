315bc1c443c226b1e1c1f937c719927c052a7a4a
Alex <websivustot@gmail.com>
2019-12-13 12:11:42 +0200

    part1 step6

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const Header = (props) => {
-    console.log(props);
+    //console.log(props);
     return (
-        <h1>{props.course.name}</h1>
+        <h1>{props.feedback.title}</h1>
     )
 }
 
@@ -12,53 +12,65 @@ const Content = (props) => {
 
     return (
         <>
-        <Part name={props.course.parts[0].name} exercise={props.course.parts[0].exercises} />
-        <Part name={props.course.parts[1].name} exercise={props.course.parts[1].exercises} />
-        <Part name={props.course.parts[2].name} exercise={props.course.parts[2].exercises} />    
-      </>
+            <Button text={props.buttons[0]} setToValue={props.setToValue}/>
+            <Button text={props.buttons[1]} setToValue={props.setToValue}/>
+            <Button text={props.buttons[2]} setToValue={props.setToValue}/> 
+        </>
     )
 }
 
 const Total = (props) => {
     return (
-        <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
+        <>
+        <h2>Statistics</h2>
+        <p>{props.feedback[0].buttons[0]}: {props.feedback[1][0]}</p>
+        <p>{props.feedback[0].buttons[1]}: {props.feedback[1][1]}</p>
+        <p>{props.feedback[0].buttons[2]}: {props.feedback[1][2]}</p>
+        </>
     )
 }
 
-const Part = (props) => {
+const Button = (props) => {
     return (
-        <p>
-            {props.name} {props.exercise}
-        </p>
+        <button onClick={() => props.setToValue(props.text)}>
+            {props.text}
+        </button>
     )
 }
 
 const App = () => {
-  const course = {
-      name: 'Half Stack application development',
-      parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
-    }  
 
-  return (
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+    //const [allClicks, setValue] = useState([0,0,0]);
+
+    const feedback = {
+        title: 'Give feedback',
+        buttons: ['Good', 'Neutral', 'Bad']
+    }
+    
+    const setToValue = newValue => {
+        if (newValue === 'Good') {
+            setGood(good + 1);
+        }
+        if (newValue === 'Bad') {
+            setBad(bad + 1);
+        }
+        if (newValue === 'Neutral') {
+            setNeutral(neutral + 1);
+        }
+        console.log(good,bad,neutral);
+    }
+
+    return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />      
+        <Header feedback={feedback} />
+        <Content buttons={feedback.buttons} setToValue={setToValue} />
+        <Total feedback={[feedback,[good,neutral,bad]]} />      
     </div>
-  )
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
