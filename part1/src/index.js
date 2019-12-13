29d87fde45fc9a3b5b465650833d5e5c1a88578d
Alex <websivustot@gmail.com>
2019-12-13 15:07:18 +0200

    part1 step13

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,109 +1,57 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-
-const Header = (props) => {
-    //console.log(props);
-    return (
-        <h1>{props.feedback.title}</h1>
-    )
-}
-
-const Content = (props) => {
-
-    return (
-        <>
-            <Button text={props.buttons[0]} setToValue={props.setToValue}/>
-            <Button text={props.buttons[1]} setToValue={props.setToValue}/>
-            <Button text={props.buttons[2]} setToValue={props.setToValue}/> 
-        </>
-    )
-}
-
-const Statistic = (props) => {
-    return (
-        <tr><td><p>{props.text}</p></td><td><p>{props.value}</p></td></tr>
-    )
-}
-
-const Statistics = (props) => {
-    if (props.feedback[1][3])  
-        return (
-            <>        
-            <h2>Statistics</h2>
-            <table>
-                <tbody>
-                <Statistic text={props.feedback[0].buttons[0]} value ={props.feedback[1][0]} />
-                <Statistic text={props.feedback[0].buttons[1]} value ={props.feedback[1][1]} />
-                <Statistic text={props.feedback[0].buttons[2]} value ={props.feedback[1][2]} />
-                <Statistic text="All" value ={props.feedback[1][3]} />
-                <Statistic text="Average" value ={props.feedback[1][4]} />
-                <Statistic text="Positive" value ={props.feedback[1][5] + "%"} />
-                </tbody>
-            </table>
-                        
-            </>
-        )
-     else return (
-        <>        
-        <h2>Statistics</h2>
-        <p>No feedback given.</p>
-        </>
-    )
-    
-}
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
 const Button = (props) => {
-    return (
-        <button onClick={() => props.setToValue(props.text)}>
-            {props.text}
-        </button>
-    )
+    
+    return (        
+        <button onClick={props.changeText}>{props.text}</button>       
+    )    
 }
 
-const App = () => {
+const App = (props) => {
+  
+    const [selected, setSelected] = useState(0);
 
-    // save clicks of each button to own state
-    const [good, setGood] = useState(0);
-    const [neutral, setNeutral] = useState(0);
-    const [bad, setBad] = useState(0);
-    const [all, setAll] = useState(0);
-    const [average, setAverage] = useState(0);
-    const [positive, setPositive] = useState(0);
+    const [points, setPoints] = useState(new Array(6).fill(0));
 
-    const feedback = {
-        title: 'Give feedback',
-        buttons: ['Good', 'Neutral', 'Bad']
+    const changeText = () => {
+        setSelected(getRandomNumber());
+    }
+
+    const getRandomNumber = () => {
+        return Math.floor(Math.random() * 6);
     }
     
-    const setToValue = newValue => {
-        if (newValue === 'Good') {
-            setGood(good + 1);
-            setAll(all + 1);
-            setAverage(average + 1);
-            setPositive((good + 1) / (all + 1) * 100);
-        }
-        if (newValue === 'Bad') {
-            setBad(bad + 1);
-            setAll(all + 1);
-            setAverage(average - 1);
-            setPositive((good) / (all + 1) * 100);
-        }
-        if (newValue === 'Neutral') {
-            setNeutral(neutral + 1);
-            setAll(all + 1);
-            setPositive((good) / (all + 1) * 100);
-        }        
-        
-        console.log(good,bad,neutral,all,average);
+    const vote = () => {
+
+        const copy = [...points];
+        copy[selected] += 1;
+        setPoints(copy);
+        console.log(points);
     }
 
-    return (
+  return (
     <div>
-        <Header feedback={feedback} />
-        <Content buttons={feedback.buttons} setToValue={setToValue} />
-        <Statistics feedback={[feedback,[good,neutral,bad,all,average,positive]]} />      
+        {props.anecdotes[selected]}
+        <div>
+        <Button text="Vote" changeText={vote} />
+        <Button text="Next anecdote" changeText={changeText} />
+        </div>        
     </div>
-    )
+    
+  )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
